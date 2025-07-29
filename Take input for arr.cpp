f19ae7c544b29f0e30b501be2388fbdc098268c8
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-07-29 14:59:54 +0530

    Create Take input for arr.cpp

---
+++ Take input for arr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int arr[5];
+
+for(int i=0 ;i<5;i++){
+    cout<<"enter a number for the array ";
+    cin>>arr[i];
+}
+
+for(int i=0;i<5;i++){
+    
+    cout<<arr[i]<<endl;
+    
+}
+
+}
+
+//Output
+//enter a number for the array10
+//enter a number for the array20
+//enter a number for the array30
+//enter a number for the array40
+//enter a number for the array50
+//10
+//20
+//30
+//40
+//50
