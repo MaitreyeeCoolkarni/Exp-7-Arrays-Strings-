da7beb142f9aa856010cbdf45cf1d131a839df8c
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-07-29 15:29:34 +0530

    Create Reverse Array.cpp

---
+++ Reverse Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//To find the number in the array.
+int main() {
+    
+int arr[5]={10,20,30,40,50};
+
+
+for(int i=0;i<5;i++){
+    cout<<arr[4-i]<<endl;
+}
+
+}
+//Output:50,40,30,20,10
