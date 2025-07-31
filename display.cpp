02cf984ea9d91949f1ab1a717306f31799a27b45
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-07-31 13:31:12 +0530

    Create display.cpp

---
+++ display.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cout << "Enter a string: ";
+    cin>>a;
+    cout<<"Entered string is: "<<a<<endl;
+
+    return 0;
+}
+/*Output
+Enter a string: Hello123
+Entered string is: Hello123*/
